bdd8e67cf76fa066063e87c86f1d52f9ace4d6e9
William Spacefire <william@meycup.com>
2021-03-29 23:16:16 -0300

    Code now look Prettier and data persists in localStorage

--- components/lib/list.js
+++ components/lib/list.js
@@ -1,35 +1,51 @@
 import { useState } from "react";
 
 export default function listLogic() {
-    
-    const [list, setList] = useState([])
+    const LocalStorage = require("localstorage");
+    var db;
 
-    let newTask = ""
+    if (process.browser) {
+        db = new LocalStorage("tasks");
+    }
+
+    const [list, setList] = useState(db ? db.get("myTask")[1] : []);
+
+    let newTask = "";
+
+    function updateListStorage() {
+        if (process.browser) {
+            const db = new LocalStorage("tasks");
+            db.put("myTask", list);
+        }
+    }
 
     function addNewTask(e) {
-        e.preventDefault()
+        e.preventDefault();
 
-        setList([{completed: false, task: newTask}, ...list])
-        console.log(JSON.stringify(list, null, 2))
+        setList([{ completed: false, task: newTask }, ...list]);
+        console.log(JSON.stringify(list, null, 2));
+        updateListStorage();
     }
 
     function updateNewtask(e) {
-        newTask = e.target.value
+        newTask = e.target.value;
     }
 
     function handleCheck(e) {
-        const id = e.target.id
-        const cheked = e.target.checked
-        list[id].completed = cheked
-        
-        setList([...list])
+        const id = e.target.id;
+        const cheked = e.target.checked;
+        list[id].completed = cheked;
+
+        setList([...list]);
+        updateListStorage();
     }
 
-   function deleteTask(e, id) {
-        let listCopy = list
-        listCopy.splice(id, 1)  
+    function deleteTask(e, id) {
+        let listCopy = list;
+        listCopy.splice(id, 1);
 
-        setList([...listCopy])
+        setList([...listCopy]);
+        updateListStorage();
     }
 
     return {
@@ -38,5 +54,5 @@ export default function listLogic() {
         updateNewtask,
         handleCheck,
         deleteTask,
-    }
+    };
 }
--- components/ui/list.js
+++ components/ui/list.js
@@ -1,77 +1,125 @@
-import { Checkbox, Divider, Fab, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, ListSubheader, Paper, TextField } from "@material-ui/core";
+import {
+    Checkbox,
+    Divider,
+    Fab,
+    IconButton,
+    List,
+    ListItem,
+    ListItemSecondaryAction,
+    ListItemText,
+    ListSubheader,
+    Paper,
+    TextField,
+} from "@material-ui/core";
 import { Add, Delete } from "@material-ui/icons";
-import listLogic from '../lib/list'
+import listLogic from "../lib/list";
 
 export default function TaskList() {
-
-    const { list, addNewTask, updateNewtask, handleCheck, deleteTask } = listLogic()
+    const {
+        list,
+        addNewTask,
+        updateNewtask,
+        handleCheck,
+        deleteTask,
+    } = listLogic();
 
     return (
         <>
-            <Fab style={{marginTop: "34px"}} color="primary" aria-label="Nova Tarefa">
-                <Add/>
+            <Fab
+                style={{ marginTop: "34px" }}
+                color="primary"
+                aria-label="Nova Tarefa">
+                <Add />
             </Fab>
-            <Paper style={{marginTop: "34px"}}>
-                <AddNewTask style={{display: "none"}}/>
-                <List 
+            <Paper style={{ marginTop: "34px" }}>
+                <AddNewTask style={{ display: "none" }} />
+                <List
                     component="nav"
                     subheader={
-                        <ListSubheader component="div" id="nested-list-subheader">
+                        <ListSubheader
+                            component="div"
+                            id="nested-list-subheader">
                             TAREFAS A FAZER
                         </ListSubheader>
                     }>
-                        <Divider/>
-                        {
-                            list.map((task, index) => {
-                                if (!task.completed)return <TaskListItem key={index} title={task.task} checked={task.completed} id={index}/>
-                            })
-                        }
+                    <Divider />
+                    {list.map((task, index) => {
+                        if (!task.completed)
+                            return (
+                                <TaskListItem
+                                    key={index}
+                                    title={task.task}
+                                    checked={task.completed}
+                                    id={index}
+                                />
+                            );
+                    })}
                 </List>
-                <List 
+                <List
                     component="nav"
                     subheader={
-                        <ListSubheader component="div" id="nested-list-subheader">
+                        <ListSubheader
+                            component="div"
+                            id="nested-list-subheader">
                             TAREFAS COMPLETADAS
                         </ListSubheader>
                     }>
-                        <Divider/>
-                        {
-                            list.map((task, index) => {
-                                if (task.completed) return <TaskListItem key={index} title={task.task} checked={task.completed} id={index}/>
-                            })
-                        }
-                    </List>
+                    <Divider />
+                    {list.map((task, index) => {
+                        if (task.completed)
+                            return (
+                                <TaskListItem
+                                    key={index}
+                                    title={task.task}
+                                    checked={task.completed}
+                                    id={index}
+                                />
+                            );
+                    })}
+                </List>
             </Paper>
         </>
-    )
+    );
 
     function AddNewTask() {
         return (
             <>
-                <ListItem style={{padding: "20px"}}>
+                <ListItem style={{ padding: "20px" }}>
                     <form action="/" onSubmit={addNewTask}>
-                        <TextField onChange={updateNewtask} style={{width: "100%"}} id="add-new-task" label="Adicionar nova tarefa" />
+                        <TextField
+                            onChange={updateNewtask}
+                            style={{ width: "100%" }}
+                            id="add-new-task"
+                            label="Adicionar nova tarefa"
+                        />
                     </form>
                 </ListItem>
             </>
-        )
+        );
     }
-        
+
     function TaskListItem({ title, checked, id }) {
         return (
             <>
                 <ListItem>
                     <ListItemText>
-                        <Checkbox checked={checked} id={id} onChange={handleCheck}/>
+                        <Checkbox
+                            checked={checked}
+                            id={id}
+                            onChange={handleCheck}
+                        />
                         {title}
                     </ListItemText>
                     <ListItemSecondaryAction>
-                        <IconButton edge="end" aria-label="delete" onClick={(e) => deleteTask(e, id)}>
+                        <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            onClick={(e) => deleteTask(e, id)}>
                             <Delete />
                         </IconButton>
-                  </ListItemSecondaryAction>
+                    </ListItemSecondaryAction>
                 </ListItem>
             </>
-        )
+        );
     }
 }
